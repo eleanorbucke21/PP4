e6ca41b1137596c76b78541bc1efbae764897cbe
Eleanor Bucke <rubybucke@live.com>
2023-01-16 17:28:02 +0000

    Added comment and Post to Models

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Post
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('content')
+
+
