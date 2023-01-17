60e430f7741542fc5e12ac9f2b7ac277f66c6376
Eleanor Bucke <rubybucke@live.com>
2023-01-17 14:28:34 +0000

    added to views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.views import generic
+from .models import Post
 # Create your views here.
+
+
+class PostList(generic.ListView):
+    model = Post
+    queryset = Post.objects.filter(status=1).order_by('-created on')
+    template_name = 'index.html'
+    paginate_by = 6
