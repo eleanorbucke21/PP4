f8e80214a1f3cf7edaba85ccb07222d4ded8f081
Eleanor Bucke <rubybucke@live.com>
2023-01-25 18:08:16 +0000

    Added Comments with approval

--- bawarchikhana/settings.py
+++ bawarchikhana/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',
     'django_summernote',
+    'crispy_forms',
     'blog',
 ]
 
@@ -58,6 +59,8 @@ SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
